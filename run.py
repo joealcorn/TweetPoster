bea8b07180e6e9b2c786dfe37e12e75090363a1c
Joe Alcorn <joe@joealcorn.co.uk>
2013-06-20 21:12:27 +0100

    Add twitter stuff to default config and allow easier merging of configs

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import os
 import json
+import sys
 
 default_conf = {
     'reddit': {
@@ -15,10 +16,20 @@ default_conf = {
 }
 
 
+def write_conf(conf):
+    config = json.dumps(conf, indent=4, sort_keys=True)
+    with open('config.json', 'w') as f:
+        f.write(config)
+
+
 if __name__ == '__main__':
     if not os.path.isfile('config.json'):
-        config = json.dumps(default_conf, indent=4, sort_keys=True)
-        with open('config.json', 'w') as f:
-            f.write(config)
-
+        write_conf(default_conf)
         print 'Created default config in config.json, please edit'
+
+    elif 'updateconf' in sys.argv:
+        with open('config.json', 'r') as f:
+            config = json.loads(f.read())
+
+        default_conf.update(config)
+        write_conf(default_conf)
