3ce0db9cccea34998674e340c1dcc7f49b487e9a
Joe Alcorn <joe@joealcorn.co.uk>
2013-07-01 17:48:04 +0100

    Convert JSON into Tweet and TwitterUser objects

--- TweetPoster/twitter.py
+++ TweetPoster/twitter.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from requests_oauthlib import OAuth1
 
 from TweetPoster import User, config
@@ -26,4 +28,24 @@ class Twitter(User):
         r = self.get(url, params=params)
         assert r.status_code == 200, r.status_code
 
-        return r.json()
+        return Tweet(r.json())
+
+
+class Tweet(object):
+
+    def __init__(self, json):
+        self.user = TwitterUser(json['user'])
+        self.text = json['text']
+        self.id = json['id']
+        self.in_reply_to = json['in_reply_to_status_id_str']
+        self.entities = json['entities']
+        self.link = 'https://twitter.com/{0}/status/{1}'.format(self.user.name, self.id)
+
+        self.datetime = datetime.strptime(json['created_at'], '%a %b %d %H:%M:%S +0000 %Y')
+
+
+class TwitterUser(object):
+
+    def __init__(self, json):
+        self.name = json['screen_name']
+        self.link = 'https://twitter.com/' + self.name
